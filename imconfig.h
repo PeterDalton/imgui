9517114d4800c695aa9a95596556cf46d9100bdd
Peter Dalton <pdalton@madprog.com>
2026-05-23 20:32:11 -0500

    [MadProg] - Updating config to match Core.

--- imconfig.h
+++ imconfig.h
@@ -112,7 +112,7 @@
         operator MyVec4() const { return MyVec4(x,y,z,w); }
 */
 //---- ...Or use Dear ImGui's own very basic math operators.
-//#define IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_DEFINE_MATH_OPERATORS
 
 //---- Use 32-bit vertex indices (default is 16-bit) is one way to allow large meshes with more than 64K vertices.
 // Your renderer backend will need to support it (most example renderer backends support both 16/32-bit indices).
@@ -145,3 +145,23 @@ namespace ImGui
     void MyFunction(const char* name, MyMatrix44* mtx);
 }
 */
+
+// Inject conversion to and from Vector[2|4] to Imgui math types
+
+namespace core
+{
+    class Vector2;
+    class Vector4;
+}
+
+#define IMGUI_DISABLE_DEFAULT_SHELL_FUNCTIONS
+#define IM_VEC2_CLASS_EXTRA \
+    explicit ImVec2(core::Vector2 const & v); \
+    operator core::Vector2() const;
+
+#define IM_VEC4_CLASS_EXTRA \
+    explicit ImVec4(core::Vector4 const & v); \
+    operator core::Vector4() const;
+
+#include "Core/Util/Assert.h" // So we can get at CORE_VERIFY_CATEGORY.
+#define IM_ASSERT(_EXPR) do { CORE_VERIFY_CATEGORY(ECodeCategory::UI, (_EXPR)); } while (0)
